1268f39bf9bfa1b83de90b61e3b867741053e72e
unknown <christophervcudiamat.13@gmail.com>
2023-11-30 16:10:56 +0800

    feat: create eslint and prettier configurations

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  arrowParens: "always",
+  singleQuote: false,
+  printWidth: 80,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  singleAttributePerLine: true,
+  useTabs: false,
+};
