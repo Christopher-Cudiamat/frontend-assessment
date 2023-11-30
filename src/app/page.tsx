bc0a831e20ec3dc0ebc03c1af58ea677ef4e7d29
unknown <christophervcudiamat.13@gmail.com>
2023-11-30 20:52:46 +0800

    feat: create card-section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
-import HeroBanner from "@/components/HeroBanner/HeroBanner";
 import React from "react";
+import cards from "../../data/cards.json";
+import HeroBanner from "@/components/HeroBanner/HeroBanner";
+import CardsSection from "@/components/CardsSection/CardsSection";
 
-const Home: React.FC = () => {
-  const mobileImage = "https://via.placeholder.com/600x600";
-  const desktopImage = "https://via.placeholder.com/1920x650";
-
+const Home = () => {
+  const mobileImage =
+    "https://fastly.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4";
+  const desktopImage =
+    "https://fastly.picsum.photos/id/9/5000/3269.jpg?hmac=cZKbaLeduq7rNB8X-bigYO8bvPIWtT-mh8GRXtU3vPc";
+  console.log("CARDS====", cards);
   return (
     <React.Fragment>
       <HeroBanner
@@ -14,6 +18,7 @@ const Home: React.FC = () => {
         title="Hello Developer!"
         subtitle="Lorem Ipsum dolor sit amet, consectetur adipiscing elit"
       />
+      <CardsSection items={cards} />
     </React.Fragment>
   );
 };
---
+++ src/components/CardsSection/CardsSection.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { Row, Col, Container } from "react-bootstrap";
+import "./styles.css";
+
+interface Item {
+  id: string;
+  image: string;
+  text: string;
+  link: string;
+}
+
+interface CardsSectionProps {
+  items: Item[];
+}
+
+const CardsSection = ({ items }: CardsSectionProps) => {
+  return (
+    <Container
+      as="section"
+      className="cards-section"
+    >
+      <Row
+        as="ul"
+        className="cards-section__container"
+      >
+        {items.map((item: Item) => (
+          <Col
+            as="li"
+            lg={4}
+            key={item.id}
+            className="cards-section__card-container"
+          >
+            <Card className="cards-section__card">
+              <div className="cards-section__card-image-container">
+                <Card.Img
+                  variant="top"
+                  src={item.image}
+                  className="cards-section__card-image"
+                />
+              </div>
+              <Card.Body className="cards-section__card-container">
+                <Card.Text className="cards-section__card-text">
+                  {item.text}
+                </Card.Text>
+                <Link
+                  href={item.link}
+                  className="cards-section__card-button"
+                >
+                  Read more
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default CardsSection;
