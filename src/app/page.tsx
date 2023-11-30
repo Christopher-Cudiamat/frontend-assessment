c133ca893938e55c9109b8a71037e1ee3ec76b83
unknown <christophervcudiamat.13@gmail.com>
2023-11-30 17:04:37 +0800

    feat: create hero-banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import HeroBanner from "@/components/HeroBanner/HeroBanner";
 import React from "react";
 
 const Home: React.FC = () => {
-  return <React.Fragment></React.Fragment>;
+  const mobileImage = "https://via.placeholder.com/600x600";
+  const desktopImage = "https://via.placeholder.com/1920x650";
+
+  return (
+    <React.Fragment>
+      <HeroBanner
+        mobileImage={mobileImage}
+        desktopImage={desktopImage}
+        altText=""
+        title="Hello Developer!"
+        subtitle="Lorem Ipsum dolor sit amet, consectetur adipiscing elit"
+      />
+    </React.Fragment>
+  );
 };
 
 export default Home;
---
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import "./styles.css";
+import useWindowSize from "@/hooks/useWindowSize";
+
+interface HeroBannerProps {
+  desktopImage: string;
+  mobileImage: string;
+  altText: string;
+  title: string;
+  subtitle: string;
+}
+const HeroBanner: React.FC<HeroBannerProps> = ({
+  desktopImage,
+  mobileImage,
+  altText,
+  title,
+  subtitle,
+}) => {
+  const windowSize = useWindowSize();
+  const desktopScreenSize = 1024;
+
+  return (
+    <section className="hero-banner">
+      <Image
+        src={windowSize.width > desktopScreenSize ? desktopImage : mobileImage}
+        alt={altText}
+        fill
+        className="hero-banner__image"
+      />
+      <div className="hero-banner__overlay" />
+      <div className="hero-banner__title-container">
+        <h1 className="hero-banner__title">{title}</h1>
+        <h2 className="hero-banner__subtitle">{subtitle}</h2>
+      </div>
+    </section>
+  );
+};
+
+export default HeroBanner;
